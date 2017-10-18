5e1f78c4b48fafcd7929eea3de121f12571cbe19
compsci <compsci@compsci>
2017-10-18 10:28:17 -0600

    Another

--- src/sandbox/runner.java
+++ src/sandbox/runner.java
@@ -1,5 +1,7 @@
 package sandbox;
 
+import java.util.Scanner;
+
 public class runner {
 
 	
@@ -13,5 +15,20 @@ public class runner {
 		
 		
 	System.out.println("bits = " + numberBits + " bytes = " + byteAnswer + " remainder = " + byteReminder);
+	
+	/*******How to handle mixed care y, Y, yes, Yes, YES...*****/
+	
+	Scanner kbd = new Scanner(System.in);
+	System.out.println("Hello, enter your answer (Y/N)");
+ 	String answer = kbd.nextLine().toUpperCase();
+    System.out.println("You have entered " + answer);
+    
+    if (answer.equalsIgnoreCase("Y") || answer.equals("YES")){
+    	System.out.println("You have selected YES!");
+    }else if (answer.equalsIgnoreCase("N") || answer.equals("NO")){
+    	System.out.println("You have selected NO!");
+    }else {
+    	System.out.println("I am soooo confused. Please enter another reponse");
+    }
 	}
 }
